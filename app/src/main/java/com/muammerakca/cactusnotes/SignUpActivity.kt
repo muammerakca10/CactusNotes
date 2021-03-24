18873369b55cb0d327cb0f2ea49facc3f08b48ef
Muammer Akca <muammerkaca10@gmail.com>
2021-03-24 09:20:42 +0300

    code shortening and spelling correction

--- app/src/main/java/com/muammerakca/cactusnotes/SignUpActivity.kt
+++ app/src/main/java/com/muammerakca/cactusnotes/SignUpActivity.kt
@@ -11,7 +11,7 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        
+
         val textInputEmail: TextInputLayout = findViewById(R.id.textInputEmail)
         val textInputUsername: TextInputLayout = findViewById(R.id.textInputUsername)
         val textInputPassword: TextInputLayout = findViewById(R.id.textInputPassword)
@@ -21,60 +21,37 @@ class SignUpActivity : AppCompatActivity() {
         //-------------------------------------------------------------------------------------------
 
         //Email Control Start
-        fun emailNullEmptyControl(email: String): Boolean {
-            return !email.isEmpty()
-        }
+        fun emailNullEmptyControl(email: String) = !email.isEmpty()
 
-        fun emailContainDotControl(email: String): Boolean {
-            return email.contains(".")
-        }
+        fun emailContainDotControl(email: String) = email.contains(".")
 
-        fun emailCannotContainSpaceControl(email: String): Boolean {
-            return !email.contains(" ")
-        }
+        fun emailCannotContainSpaceControl(email: String) = !email.contains(" ")
 
-        fun emailContainAtControl(email: String): Boolean {
-            return email.contains("@")
-        }
+        fun emailContainAtControl(email: String) = email.contains("@")
 
-        fun emailMinCharacterControl(email: String): Boolean {
-            return email.length > 5
-        }
+        fun emailMinCharacterControl(email: String) = email.length > 5
 
-        fun emailMaxCharacterControl(email: String): Boolean {
-            return email.length < 50
-        } // Email Control End
+        fun emailMaxCharacterControl(email: String) = email.length < 50
+        // Email Control End
 
         //Email Error Messages Start
         fun emailErrorMessages(email: String) = when {
             !emailNullEmptyControl(email) -> "Email is required."
-            !emailContainDotControl(email) -> "Email is invalid."
-            !emailContainAtControl(email) -> "Email is invalid."
-            !emailMinCharacterControl(email) -> "Email is invalid."
-            !emailMaxCharacterControl(email) -> "Email is invalid."
+            !emailContainDotControl(email) || !emailContainAtControl(email) || !emailMinCharacterControl(email) || !emailMaxCharacterControl(email) -> "Email is invalid."
             !emailCannotContainSpaceControl(email) -> "Email cannot contain space"
             else -> null
         } //Email Error Messages Finish
 
-        //-------------------------------------------------------------------------------------------
-
         //Username Controls Start
-        fun usernameNullEmptyControl(username: String): Boolean {
-            return !username.isEmpty()
-        }
+        fun usernameNullEmptyControl(username: String) = !username.isEmpty()
 
-        fun usernameEligibleCharactersControl(username: String): Boolean {
-            return username.all { it.isDigit() || it.isLowerCase() || it == '_' }
-            //username!!.contains("[a-z][0-9]_") seklinde de yazilabilir mi?
-        }
+        fun usernameEligibleCharactersControl(username: String) =
+            username.all { it.isDigit() || it.isLowerCase() || it == '_' }
 
-        fun usernameMinCharacterControl(username: String): Boolean {
-            return username.length > 2
-        }
+        fun usernameMinCharacterControl(username: String) = username.length > 2
 
-        fun usernameMaxCharacterControl(username: String): Boolean {
-            return username.length < 20
-        } //Username Controls Finish
+        fun usernameMaxCharacterControl(username: String) = username.length < 20
+        //Username Controls Finish
 
         //Username Error Messages Start
         fun usernameErrorMessages(username: String) = when {
@@ -83,63 +60,40 @@ class SignUpActivity : AppCompatActivity() {
             !usernameMinCharacterControl(username) -> "Username is too short."
             !usernameMaxCharacterControl(username) -> "Username is too long."
             else -> null
-
         } //Username Error Messages Finish
 
-        //-------------------------------------------------------------------------------------------
-
         //Password Controls Start
-        fun passwordNullOrEmptyControl(password: String): Boolean {
-            return !password.isNullOrEmpty()
-        }
+        fun passwordNullOrEmptyControl(password: String) = !password.isNullOrEmpty()
 
-        fun passwordDigitControl(password: String): Boolean {
-            return password.any { it.isDigit() }
-        }
+        fun passwordDigitControl(password: String) = password.any { it.isDigit() }
 
-        fun passwordUpperCaseControl(password: String): Boolean {
-            return password.any { it.isUpperCase() }
-        }
+        fun passwordUpperCaseControl(password: String) = password.any { it.isUpperCase() }
 
-        fun passwordLowerCaseControl(password: String): Boolean {
-            return password.any { it.isLowerCase() }
-        }
+        fun passwordLowerCaseControl(password: String) = password.any { it.isLowerCase() }
 
-        fun passwordLetterOrDigitCharacterControl(password: String): Boolean {
-            return password.any { it.isLetterOrDigit() }
-        }
+        fun passwordLetterOrDigitCharacterControl(password: String) = password.any { it.isLetterOrDigit() }
 
-        fun passwordMinCharacterControl(password: String): Boolean {
-            return password.length > 7
-        }
+        fun passwordMinCharacterControl(password: String) = password.length > 7
 
-        fun passwordMaxCharacterControl(password: String): Boolean {
-            return password.length < 40
-        }
+        fun passwordMaxCharacterControl(password: String) = password.length < 40
 
-        fun passwordSpaceControl(password: String): Boolean {
-            return !password.contains(" ")
-        } //Password Controls Finish
+        fun passwordSpaceControl(password: String) = !password.contains(" ")
+        //Password Controls Finish
 
         //Password Error Messages Start
         fun passwordErrorMessages(password: String) = when {
             !passwordNullOrEmptyControl(password) -> "Password is required."
-            !passwordDigitControl(password) -> "Password must contain one digit, one uppercase letter, one lowercase letter and one special character."
-            !passwordUpperCaseControl(password) -> "Password must contain one digit, one uppercase letter, one lowercase letter and one special character."
-            !passwordLowerCaseControl(password) -> "Password must contain one digit, one uppercase letter, one lowercase letter and one special character."
-            !passwordLetterOrDigitCharacterControl(password) -> "Password must contain one digit, one uppercase letter, one lowercase letter and one special character."
+            !passwordDigitControl(password) || !passwordUpperCaseControl(password) || !passwordLowerCaseControl(password) || !passwordLetterOrDigitCharacterControl(password) -> "Password must contain one digit, one uppercase letter, one lowercase letter and one special character."
             !passwordMinCharacterControl(password) -> "Password is too short."
             !passwordMaxCharacterControl(password) -> "Password is too long."
             !passwordSpaceControl(password) -> "Password cannot contain space."
             else -> null
         } //Password Error Messages Finish
 
-        //-------------------------------------------------------------------------------------------
-
         //Button Click Start
         signupButton.setOnClickListener() {
             var email =
-                textInputEmail.editText!!.text.toString() //bosluklara hassasiyeti artirmak icin trimleri kaldirdim
+                textInputEmail.editText!!.text.toString()
 
             var username = textInputUsername.editText!!.text.toString()
             val password = textInputPassword.editText!!.text.toString()
