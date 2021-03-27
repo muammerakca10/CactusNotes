3b658820ea7615f1d2d3336bae3211139ce94bc6
Muammer Akca <muammerkaca10@gmail.com>
2021-03-27 17:26:16 +0300

    Codes shortening and bug fixes

--- app/src/main/java/com/muammerakca/cactusnotes/SignUpActivity.kt
+++ app/src/main/java/com/muammerakca/cactusnotes/SignUpActivity.kt
@@ -18,34 +18,41 @@ class SignUpActivity : AppCompatActivity() {
         val signupButton: MaterialButton = findViewById(R.id.signupButton)
         val alreadySignButton: Button = findViewById(R.id.alreadySignButton)
 
-        //Email Error Messages Start
-        fun emailErrorMessages(email: String) = when {
+        fun validateEmail(email: String) = when {
             email.isEmpty() -> getString(R.string.email_required)
             !email.contains(".") || !email.contains("@") || email.length <= 5 || email.length >= 50 -> getString(R.string.email_invalid)
             email.contains(" ") -> getString(R.string.email_cannot_space)
             else -> null
-        } //Email Error Messages Finish
+        }
 
-        //Username Error Messages Start
-        fun usernameErrorMessages(username: String) = when {
+        fun validateUsername(username: String) = when {
             username.isEmpty() -> getString(R.string.username_required)
             !username.all { it.isDigit() || it.isLowerCase() || it == '_' } -> getString(R.string.username_only_this_characters)
             username.length <= 2 -> getString(R.string.username_short)
             username.length >= 20 -> getString(R.string.username_long)
             else -> null
-        } //Username Error Messages Finish
+        }
 
-        //Password Error Messages Start
-        fun passwordErrorMessages(password: String) = when {
+        fun String.passwordHasDigit() = any { it.isDigit() }
+
+        fun String.passwordHasUpperCase() = any { it.isUpperCase() }
+
+        fun String.passwordHasLowerCase() = any { it.isLowerCase() }
+
+        fun String.passwordHasSpecialCharacters() = any { !it.isLetterOrDigit() }
+
+        fun validatePassword(password: String) = when {
             password.isEmpty() -> getString(R.string.password_required)
-            !password.any { it.isDigit() } || !password.any { it.isDigit() } || !password.any { it.isLowerCase() } || !password.any { it.isLetterOrDigit() } -> getString(R.string.password_must_contains_character)
+            !password.passwordHasDigit()
+                    || !password.passwordHasUpperCase()
+                    || !password.passwordHasLowerCase()
+                    || !password.passwordHasSpecialCharacters() -> getString(R.string.password_must_contains_character)
             password.length < 8 -> getString(R.string.password_short)
             password.length >= 40 -> getString(R.string.password_long)
             password.contains(" ") -> getString(R.string.password_cannot_space)
             else -> null
-        } //Password Error Messages Finish
+        }
 
-        //Button Click Start
         signupButton.setOnClickListener {
             var email =
                 textInputEmail.editText!!.text.toString()
@@ -53,20 +60,20 @@ class SignUpActivity : AppCompatActivity() {
             var username = textInputUsername.editText!!.text.toString()
             val password = textInputPassword.editText!!.text.toString()
 
-            val emailHasError = emailErrorMessages(email) != null
+            val emailHasError = validateEmail(email) != null
             textInputEmail.isErrorEnabled = emailHasError
-            textInputEmail.error = emailErrorMessages(email)
+            textInputEmail.error = validateEmail(email)
 
-            val usernameHasError = usernameErrorMessages(username) != null
+            val usernameHasError = validateUsername(username) != null
             textInputUsername.isErrorEnabled = usernameHasError
-            textInputUsername.error = usernameErrorMessages(username)
+            textInputUsername.error = validateUsername(username)
 
-            val passwordHasError = passwordErrorMessages(password) != null
+            val passwordHasError = validatePassword(password) != null
             textInputPassword.isErrorEnabled = passwordHasError
-            textInputPassword.error = passwordErrorMessages(password)
+            textInputPassword.error = validatePassword(password)
 
             if (!emailHasError && !usernameHasError && !passwordHasError)
                 Toast.makeText(this, getString(R.string.successful), Toast.LENGTH_LONG).show()
-        } //Button Click End
+        }
     }
 }
